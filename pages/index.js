d0784f249edc5f2686c44a9a853236cb614d1ab0
tislik3201 <teslenko_vladislav@mail.ru>
2022-11-22 17:58:55 +0300

    addedAnimation

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import react from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [activeClass, setActiveClass] = react.useState(false)
+  const onSignUpClick = () => {
+    setActiveClass(true)
+  }
+  const onSignInClick = () => {
+    setActiveClass(false)
+  }
+
   return (
     <div>
-      <h2>Weekly Coding Challenge #1: Sign in/up Form</h2>
-<div className={styles.container} id="container">
-	<div className="form-container sign-up-container">
+      <h2>Sign in/up Form</h2>
+<div className={`${styles.container} ${activeClass ? styles.right_panel_active: ''}`} id="container">
+	<div className={`${styles.form_container} ${styles.sign_up_container}`}>
 		<form action="#">
 			<h1>Create Account</h1>
 			<div className={styles.social_container}>
@@ -40,12 +49,12 @@ export default function Home() {
 			<div className={`${styles.overlay_panel} ${styles.overlay_left}`}>
 				<h1>Welcome Back!</h1>
 				<p>To keep connected with us please login with your personal info</p>
-				<button className={styles.ghost} id="signIn">Sign In</button>
+				<button onClick={() => onSignInClick()} className={styles.ghost} id="signIn">Sign In</button>
 			</div>
 			<div className={`${styles.overlay_panel} ${styles.overlay_right}`}>
 				<h1>Hello, Friend!</h1>
 				<p>Enter your personal details and start journey with us</p>
-				<button className={styles.ghost} id="signUp">Sign Up</button>
+				<button onClick={()=> onSignUpClick()} className={styles.ghost} id="signUp">Sign Up</button>
 			</div>
 		</div>
 	</div>
